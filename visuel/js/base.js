eb536edf5a6e4a86f880de3a13866b9aa6cd30db
Benazeth Léo <leo.benazeth@ynov.com>
2024-05-07 10:03:13 +0200

    création fichier .js pour le code js

---
+++ visuel/js/base.js
@@ -0,0 +1,19 @@
+function obtenirArtistes() {
+    fetch('/artist')
+      .then(response => response.json())
+      .then(artistes => {
+            document.getElementById('listeArtistes').innerHTML = '';
+            artistes.forEach(artiste => {
+                const elementArtiste = document.createElement('div');
+                elementArtiste.classList.add('slot');
+                elementArtiste.innerHTML = `
+                    <h2>${artiste.name}</h2>
+                    <img src="${artiste.image}" alt="${artiste.name}">
+                    <a href="artist.html">voir plus</a>
+                `;
+                document.getElementById('listeArtistes').appendChild(elementArtiste);
+            });
+        })
+      .catch(error => console.error('Erreur lors de la récupération des artistes :', error));
+}
+window.onload = obtenirArtistes;
