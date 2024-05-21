3057a8cf85b76bc1c81a7f8c1ba2dbec0614903c
Benazeth Léo <leo.benazeth@ynov.com>
2024-05-21 11:53:57 +0200

    artist.html terminé

--- API/artist.go
+++ API/artist.go
@@ -7,7 +7,7 @@ type Artist struct {
 	Name           string   `json:"name"`          // Nom de l'artiste
 	Members        []string `json:"members"`       // Liste des membres de l'artiste (s'il s'agit d'un groupe)
 	CreationDate   uint16   `json:"creationDate"`  // Année de création de l'artiste
-	FirstAlbum     string   `json:"firsAlbum"`     // Premier album de l'artiste
+	FirstAlbum     string   `json:"firstAlbum"`     // Premier album de l'artiste
 	LocationsUrl   string   `json:"locations"`     // URL pour obtenir les lieux associés à l'artiste
 	ConcertDatesUrl string   `json:"concertDates"` // URL pour obtenir les dates de concerts de l'artiste
 	RelationsUrl   string   `json:"relations"`     // URL pour obtenir les relations de l'artiste avec d'autres artistes
--- API/date.go
+++ API/date.go
@@ -2,6 +2,5 @@ package API
 
 // Date représente une structure contenant un identifiant et une liste de dates
 type Date struct {
-	Id    uint64   `json:"id"`     // Identifiant de la date
 	Dates []string `json:"dates"`  // Liste des dates au format texte
 }
