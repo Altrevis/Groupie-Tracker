a99e65d6361ed8d437cb12df8c22cfbcea311ad9
Alexandre Queudeville <alexandre.queudeville@gmail.com>
2024-05-21 11:43:08 +0200

    affichage artist.html

--- controller/controller.go
+++ controller/controller.go
@@ -1,71 +1,71 @@
 package controller
 
 import (
-	"encoding/json"
-	"fmt"
-	"../service" // Import relatif, assurez-vous que la structure du projet est correcte
-	"html/template"
-	"net/http"
-	"strconv"
+    "encoding/json"
+    "fmt"
+    "../service"
+    "html/template"
+    "net/http"
+    "strconv"
 )
 
 // handle404 gère les requêtes aboutissant à une erreur 404
 func handle404(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(404)
+    w.WriteHeader(404)
 
-	t, err := template.ParseFiles("templates/base.html")
+    t, err := template.ParseFiles("templates/base.html")
 
-	if err != nil {
-		handle500(w, err) // En cas d'erreur de template, gérer avec une erreur 500
-		return
-	}
+    if err != nil {
+        handle500(w, err)
+        return
+    }
 
-	err = t.Execute(w, nil)
+    err = t.Execute(w, nil)
 
-	if err != nil {
-		handle500(w, err) // En cas d'erreur d'exécution de template, gérer avec une erreur 500
-		return
-	}
+    if err != nil {
+        handle500(w, err)
+        return
+    }
 }
 
 // handle400 gère les requêtes aboutissant à une erreur 400
 func handle400(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(400)
+    w.WriteHeader(400)
 
-	t, err := template.ParseFiles("templates/base.html")
+    t, err := template.ParseFiles("templates/base.html")
 
-	if err != nil {
-		handle500(w, err) // En cas d'erreur de template, gérer avec une erreur 500
-		return
-	}
+    if err != nil {
+        handle500(w, err)
+        return
+    }
 
-	err = t.Execute(w, nil)
+    err = t.Execute(w, nil)
 
-	if err != nil {
-		handle500(w, err) // En cas d'erreur d'exécution de template, gérer avec une erreur 500
-		return
-	}
+    if err != nil {
+        handle500(w, err)
+        return
+    }
 }
 
 // handle500 gère les erreurs internes du serveur
 func handle500(w http.ResponseWriter, err error) {
-	w.WriteHeader(500)
+    w.WriteHeader(500)
 
-	t, other := template.ParseFiles("templates/base.html")
+    t, other := template.ParseFiles("templates/base.html")
 
-	if other != nil {
-		w.Write([]byte("Something went wrong\nError 500\n" + other.Error())) // Si une erreur survient lors du chargement du template de base
-		return
-	}
+    if other != nil {
+        w.Write([]byte("Something went wrong\nError 500\n" + other.Error()))
+        return
+    }
 
-	fmt.Println(err) // Affiche l'erreur dans les logs du serveur
-	t.Execute(w, err) // Affiche l'erreur à l'utilisateur
+    fmt.Println(err)
+    t.Execute(w, err)
 }
 
 // Get gère les requêtes GET
 func Get(w http.ResponseWriter, r *http.Request) {
-    id := r.URL.Path[len("/artist/"):]
-    if len(id) == 0 {
+    id := r.URL.Query().Get("id")
+    if id == "" {
         w.Header().Set("Content-Type", "application/json")
         if err := getArtists(w); err != nil {
             handle500(w, err)
@@ -84,17 +84,8 @@ func Get(w http.ResponseWriter, r *http.Request) {
                 handle500(w, err)
                 return
             }
-            t, err := template.ParseFiles("templates/base.html", "templates/artist.html")
-            if err != nil {
-                handle400(w, r)
-                return
-            }
-
-            err = t.Execute(w, artist)
-            if err != nil {
-                handle400(w, r)
-                return
-            }
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(artist)
         } else {
             handle404(w, r)
             return
@@ -104,45 +95,45 @@ func Get(w http.ResponseWriter, r *http.Request) {
 
 // MainPage gère la page d'accueil
 func MainPage(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		t, err := template.ParseFiles("templates/base.html")
+    if r.URL.Path == "/" {
+        t, err := template.ParseFiles("templates/base.html")
 
-		if err != nil {
-			handle500(w, err)
-			return
-		}
-		artists, err := service.Get()
-		if err != nil {
-			handle500(w, err)
-			return
-		}
-		err = t.Execute(w, artists)
-		if err != nil {
-			handle500(w, err)
-			return
-		}
-	} else {
-		handle404(w, r) // Si la route demandée n'existe pas, renvoyer une erreur 404
-		return
-	}
+        if err != nil {
+            handle500(w, err)
+            return
+        }
+        artists, err := service.Get()
+        if err != nil {
+            handle500(w, err)
+            return
+        }
+        err = t.Execute(w, artists)
+        if err != nil {
+            handle500(w, err)
+            return
+        }
+    } else {
+        handle404(w, r)
+        return
+    }
 }
 
 // getArtists récupère la liste des artistes et les renvoie au format JSON
 func getArtists(w http.ResponseWriter) error {
-	artists, err := service.Get()
-	if err != nil {
-		return err // Si une erreur survient lors de la récupération des artistes, renvoyer l'erreur
-	}
+    artists, err := service.Get()
+    if err != nil {
+        return err
+    }
 
-	artistsJson, err := json.Marshal(artists)
-	if err != nil {
-		return err // Si une erreur survient lors de la conversion en JSON, renvoyer l'erreur
-	}
+    artistsJson, err := json.Marshal(artists)
+    if err != nil {
+        return err
+    }
 
-	_, err = w.Write(artistsJson)
-	if err != nil {
-		return err // Si une erreur survient lors de l'écriture de la réponse, renvoyer l'erreur
-	}
+    _, err = w.Write(artistsJson)
+    if err != nil {
+        return err
+    }
 
-	return nil
+    return nil
 }
--- visuel/js/base.js
+++ visuel/js/base.js
@@ -1,7 +1,7 @@
 function obtenirArtistes() {
     fetch('/artist')
-      .then(response => response.json())
-      .then(artistes => {
+        .then(response => response.json())
+        .then(artistes => {
             document.getElementById('listeArtistes').innerHTML = '';
             artistes.forEach(artiste => {
                 const elementArtiste = document.createElement('div');
@@ -14,6 +14,48 @@ function obtenirArtistes() {
                 document.getElementById('listeArtistes').appendChild(elementArtiste);
             });
         })
-      .catch(error => console.error('Erreur lors de la récupération des artistes :', error));
+        .catch(error => console.error('Erreur lors de la récupération des artistes :', error));
 }
-window.onload = obtenirArtistes;
+
+function obtenirArtiste(artistId) {
+    fetch(`/artist?id=${artistId}`)
+        .then(response => response.json())
+        .then(artiste => {
+            document.getElementById('artist-name').textContent = artiste.name;
+            document.getElementById('artist-image').src = artiste.image;
+            document.getElementById('creation-date').textContent = artiste.creationDate;
+            document.getElementById('first-album').textContent = artiste.firstAlbum;
+
+            const membersList = document.getElementById('members-list');
+            membersList.innerHTML = '';
+            artiste.members.forEach(member => {
+                const li = document.createElement('li');
+                li.textContent = member;
+                membersList.appendChild(li);
+            });
+
+            const concertDates = document.getElementById('concert-dates');
+            concertDates.innerHTML = '';
+            for (const date in artiste.concertDates) {
+                const li = document.createElement('li');
+                li.classList.add('collection-item');
+                li.innerHTML = `
+                    ${date}
+                    <span class="secondary-content location">${artiste.concertDates[date]}</span>
+                `;
+                concertDates.appendChild(li);
+            }
+        })
+        .catch(error => console.error('Erreur lors de la récupération des détails de l\'artiste :', error));
+}
+
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const artistId = urlParams.get('id');
+
+    if (artistId) {
+        obtenirArtiste(artistId);
+    } else {
+        obtenirArtistes();
+    }
+};
